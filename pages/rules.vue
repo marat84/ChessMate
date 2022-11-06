340bc91cbc409fcae59d4e92bc3166245d8882af
marat84 <46667902+marat84@users.noreply.github.com>
2022-11-06 10:24:26 +0500

    Изменил текст на странице Правила

--- pages/rules.vue
+++ pages/rules.vue
@@ -3,7 +3,7 @@
     <div v-html="$t('rulesContent')"></div>
     <a href="/Docs/Nizom.pdf" class="file-download" target="_blank">
       <img src="/images/pdf.svg" alt="иконка скачивания" aria-hidden="true" role="presentation">
-      {{ ($i18n.locale === 'rus') ? 'Положение на узбекском.pdf' : 'Nizom.pdf' }}
+      {{ ($i18n.locale === 'rus') ? 'Положение (на узб. яз.).pdf' : 'Nizom.pdf' }}
     </a>
   </section>
 </template>
